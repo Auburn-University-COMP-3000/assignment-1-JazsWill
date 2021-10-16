7bced25095117c0e10e1d8b23e718e8181e38101
jaw0119 <92323456+JazsWill@users.noreply.github.com>
2021-10-16 14:16:57 -0500

    Update q5.cpp

--- assignment_1/q5.cpp
+++ assignment_1/q5.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-
 using namespace std; 
 
-int main (){
+int main ()
+{
+// Assignment #1 - Problem 5
+	cout << "NUMBER 5\n";
 
-    
+	int degreesFarenheit;
+	int degreesCelsius;
+
+	degreesCelsius = 100;
+
+	do
+	{
+		degreesFarenheit = 32 + (9.0 / 5.0) * degreesCelsius;
+		if (degreesCelsius == degreesFarenheit)
+			cout << "The Celsius value " << degreesCelsius << " is the same temperature in Farenheit.\n";
+		else
+			degreesCelsius--;
+	} while (degreesCelsius != degreesFarenheit);
+
+	return 0;    
 }
