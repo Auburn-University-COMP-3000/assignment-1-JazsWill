d7851f0f5c80bfa03f8c3dcc8529f1ecaf223372
jaw0119 <92323456+JazsWill@users.noreply.github.com>
2021-10-16 14:16:01 -0500

    Update q4.cpp

--- assignment_1/q4.cpp
+++ assignment_1/q4.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-
 using namespace std; 
 
-int main (){
+int main ()
+{
+// Assignment #1 - Number 4
+	cout << "NUMBER 4\n";
 
+	int numberOfPounds;
+	int radiusOfSphere;
+
+	cout << "Input weight of the sphere in pounds:";
+	cin >> numberOfPounds;
+
+	cout << "Input the radius of the sphere in feet:";
+	cin >> radiusOfSphere;
+
+	double buoyantForce;
+	double objectVolume;
+	const double pi = 3.14;
+	const double specificWeightWater = 62.4;      /* Units: lb/cubic foot */
+
+	objectVolume = (4.0 / 3) * pi * (radiusOfSphere * radiusOfSphere * radiusOfSphere);
+	buoyantForce = objectVolume * specificWeightWater;
+
+	if (buoyantForce >= numberOfPounds)
+		cout << "The sphere will float in water.\n\n";
+	else
+		cout << "The sphere will sink in water.\n\n";
     
 }
